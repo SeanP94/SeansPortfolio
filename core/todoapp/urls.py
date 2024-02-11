ab6a3cad3b54e879348c28cba9bb40fecd408b02
seanperry <seanep94@gmail.com>
2024-02-11 11:08:30 -0800

    HTMX test working

--- core/todoapp/urls.py
+++ core/todoapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='homepage')
+    path('', views.home, name='homepage'),
+    path('testClick/', views.clickTest)
+
 ]
--- core/todoapp/views.py
+++ core/todoapp/views.py
@@ -3,10 +3,19 @@ from django.http import HttpResponse
 from django.template import loader
 # Create your views here.
 
+context = {
+        "tasks" : [f"Task: {x}" for x in range(1,16)]
+    }
+
 def home(request):
     # template = loader.get_template('todo.html')
     # print(template)
-    context = {
-        "tasks" : [f"Task: {x}" for x in range(1,16)]
-    }
+    
     return HttpResponse(render(request, 'todo.html', context=context))
+
+
+def clickTest(request):
+    '''Test to just do a click for htmx-get'''
+    
+    print("Test Successful!")
+    return HttpResponse(render(request, 'todo-list.html', context=context))
