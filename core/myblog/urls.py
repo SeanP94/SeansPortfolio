8d7bee1c392ffe826d627e02086606913686394a
seanperry <seanep94@gmail.com>
2024-03-25 13:36:10 -0700

    - Fixed Resume and homepage formatting issues - Reworded Resume so it should be up to date - Redid navbar, so it defaults to all Homepage, Except 'Web Resume'

--- core/myblog/urls.py
+++ core/myblog/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='homepage'),
+    path('', views.home, name='Homepage'),
+    path('resume/', views.resume, name='ResumePage'),
+    path('404/', views.My404_view)
 ]
+
+
+handler404 = 'myblog.views.My404_view'
--- core/myblog/views.py
+++ core/myblog/views.py
@@ -6,3 +6,10 @@ from django.http import HttpResponse, HttpRequest
 
 def home(request: HttpRequest):
     return HttpResponse(render(request, 'homepage.html', {}))
+
+
+def resume(request: HttpRequest):
+    return HttpResponse(render(request, 'resume.html', {}))
+
+def My404_view(request: HttpRequest, exception):
+    return HttpResponse(render(request, '404.html', {}))
