261f25375262fdaecd279960b320fc9b27cd62fc
seanperry <seanep94@gmail.com>
2024-04-04 16:34:14 -0700

    Cleaned up the TODO files, as theyre not much of a reference anymore

--- core/core/urls.py
+++ core/core/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('todoold', include('todoapp.urls')), # Not needed anymore, not removing because its a good reference.
     path('', include('myblog.urls'))          # Main blog project.
 ]
